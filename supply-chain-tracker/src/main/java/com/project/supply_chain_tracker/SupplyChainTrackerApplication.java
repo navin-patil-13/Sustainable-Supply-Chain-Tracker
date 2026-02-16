b7d5f3b60ee2f2e4587868a4bcc244a224b3e66a
Navin Patil <navin-patil-13@users.noreply.github.com>
2026-02-16 09:19:20 +0530

    Inital Project Setup

---
+++ supply-chain-tracker/src/main/java/com/project/supply_chain_tracker/SupplyChainTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.supply_chain_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyChainTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyChainTrackerApplication.class, args);
+	}
+
+}
---
+++ supply-chain-tracker/src/test/java/com/project/supply_chain_tracker/SupplyChainTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.supply_chain_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyChainTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
